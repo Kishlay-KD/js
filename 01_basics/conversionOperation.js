58b402e7d1686dbe46d7db73ed1f62c589cb8b0d
Kishlay Kumar Dubey <kishlaydubey455@gmail.com>
2026-08-04 08:48:37 +0000

    conversionOperation

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "kd"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1;false = 0
+let IsLogIn = "kd"
+// let booleanIsLogIn = boolean(IsLogIn)
+// console.log(booleanIsLogIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
