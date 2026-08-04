ffad9daafbcf029cd921b40815e2a112d219b517
Kishlay Kumar Dubey <kishlaydubey455@gmail.com>
2026-08-04 09:28:49 +0000

    conversionOperation

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -15,5 +15,33 @@ let IsLogIn = "kd"
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+                                    //  operations
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3/3);
+
+let str1 = "hello"
+let str2 = "  kd"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+"");
+// console.log(+true);
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
