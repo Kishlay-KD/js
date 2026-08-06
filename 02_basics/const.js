01f038c3be3399829b4a47a111a581ea712284b1
Kishlay Kumar Dubey <kishlaydubey455@gmail.com>
2026-08-06 19:34:26 +0000

    json

--- 02_basics/const.js
+++ 02_basics/const.js
@@ -24,10 +24,24 @@ const obj3 = {5: "d", 6: "f"};
 const obj4 = { ...obj1, ...obj2, ...obj3 };
 
 // console.log(obj4);
-console.log(Object.keys(user))
-console.log(Object.values(user))
-console.log(Object.entries(user))
+// console.log(Object.keys(user))
+// console.log(Object.values(user))
+// console.log(Object.entries(user))
 
+const course = {
+    coursename : "js",
+    courseprice : 999,
+    courseteacher : "kd"
+}
+const {courseteacher} = course
+console.log(courseteacher)
+
+// json
+// {
+//     "name":"ks",
+//     "age":"twenty",
+//     "fee":"free"
+// }
 
 
 
