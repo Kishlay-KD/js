9990ea6d77f991513010beaeacdfa4f8fec1aeb7
Kishlay Kumar Dubey <kishlaydubey455@gmail.com>
2026-08-07 12:55:10 +0000

    scope

---
+++ 03_basics/scope.js
@@ -0,0 +1,29 @@
+let a = 10
+if(true){
+    let a = 100
+    const b = 200
+    console.log(a)
+    
+}
+console.log(a)
+
+function one(){
+    const user = "kishlay"
+    function two(){
+        const website = "youtube"
+        console.log(user)
+
+    }
+    // two()
+}
+// one()
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function addone(num){
+    return num + 1
+}
+console.log(addone(5))
+
+const addtwo = function(num){
+    return num+2
+}
+addtwo(8)
