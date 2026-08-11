6028552a4eee71cf81ebd15e35c01f2c7b4193a2
Kishlay Kumar Dubey <kishlaydubey455@gmail.com>
2026-08-11 18:20:18 +0000

    controlflow

--- 03_basics/controlflow.js
+++ 03_basics/controlflow.js
@@ -1,1 +1,13 @@
 // loop
+switch(month){
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("february")
+        break;
+    case 3:
+        console.log("march")
+        break;
+
+}
