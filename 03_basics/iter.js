9de75f143450a6daed8f9852198e50838996d92e
Kishlay Kumar Dubey <kishlaydubey455@gmail.com>
2026-08-12 12:39:23 +0000

    iter

---
+++ 03_basics/iter.js
@@ -0,0 +1,18 @@
+const coding = ["js","py","cpp"]
+const values = coding.forEach((item)=>{
+    console.log(item)
+    return item
+})
+// console.log(values)
+
+const mynums = [1,2,3,4,5,6,7,8,9]
+// const newnums = mynums.filter((num) => num>5)
+// console.log(newnums)
+
+const newnums = []
+mynums.forEach((num) => {
+    if(num>4){
+        newnums.push(num)
+    }
+})
+console.log(newnums)
